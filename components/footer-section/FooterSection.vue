6d6760664910adcea70e6723595bade13db7d8ea
Evgenii Sergeev <evgenii.sergeev@me.com>
2024-01-24 15:59:42 +0100

    fix(layout): add alts to images

--- components/footer-section/FooterSection.vue
+++ components/footer-section/FooterSection.vue
@@ -5,18 +5,18 @@
                 Trusted by the Best Arbitration Teams Worldwide
             </p>
             <div class="footer__logos">
-                <img src="/images/logos/dla-piper.svg" />
-                <img src="/images/logos/curtis.svg" />
-                <img src="/images/logos/burford.svg" />
-                <img src="/images/logos/freshfields.svg" />
-                <img src="/images/logos/shearman-sterling.svg" />
-                <img src="/images/logos/commonwealth.svg" />
-                <img src="/images/logos/foley.svg" />
-                <img src="/images/logos/eversheds.svg" />
-                <img src="/images/logos/mofa.svg" />
-                <img src="/images/logos/dentons.svg" />
-                <img src="/images/logos/three-crowns.svg" />
-                <img src="/images/logos/gibson-dunn.svg" />
+                <img src="/images/logos/dla-piper.svg" alt="dla logo"/>
+                <img src="/images/logos/curtis.svg" alt="curtis logo"/>
+                <img src="/images/logos/burford.svg" alt="burford log"/>
+                <img src="/images/logos/freshfields.svg" alt="freshfields logo"/>
+                <img src="/images/logos/shearman-sterling.svg" alt="shearman-sterling logo"/>
+                <img src="/images/logos/commonwealth.svg" alt="commonwealth logo"/>
+                <img src="/images/logos/foley.svg" alt="foley logo"/>
+                <img src="/images/logos/eversheds.svg" alt="eversheds log"/>
+                <img src="/images/logos/mofa.svg" alt="mofa logo"/>
+                <img src="/images/logos/dentons.svg" alt="dentons logo"/>
+                <img src="/images/logos/three-crowns.svg" alt="three crowns logo"/>
+                <img src="/images/logos/gibson-dunn.svg" alt="gibson-dunn logo"/>
             </div>
         </div>
     </main>
